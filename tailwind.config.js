c18f7cd477d502ac242346069e4d5213a5a867a4
mohammad-afaque <gct.175217@gmail.com>
2020-07-24 22:43:18 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'red',
+        secondary: {
+          100: '#E2E2D5',
+          200: '888883',
+        },
+      },
+      fontFamily: {
+        body: ['Nunito']
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
